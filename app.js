37af76eb3d79c7ed59aaa48f3ea8b92cbe8a1fb0
ujjwalsharma97 <ujjwal.sharma@hoolime.co>
2019-09-14 17:26:45 +0530

    route

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 
+const bookRouter = express.Router()
 const port = process.env.PORT || 3000;
+
+
+
+bookRouter.route('/books')
+    .get((req,res) => {
+        const response = { Hello : 'This is my API'}
+        res.json(response)
+    })
+app.use('/api', bookRouter)
 
 app.get('/', (req, res) => {
     res.send("Running fine with nodemon")
