527600c0eb837dbbc11a688ff6cc73fcaecc0ce1
ujjwalsharma97 <ujjwal.sharma@hoolime.co>
2019-09-14 17:15:32 +0530

    nodemon added

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/' ,(req,res) => {
-    res.send("Welome to my page")
+app.get('/', (req, res) => {
+    res.send("Running fine with nodemon")
 })
 
 app.listen(port, () => {
-    console.log("App is running on port" + port)
+    console.log(`App is running on port  ${port}`)
 })
 
 
